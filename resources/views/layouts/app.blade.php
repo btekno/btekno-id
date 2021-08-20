22bce0b7f9558d7a06ce1e6b5b049c7a66f6b1df
Noviyanto Rahmadi <novay@enterwind.com>
2021-08-20 23:04:48 +0800

    New Package
    - HTMLMin
    - Minify

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,27 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <!-- Favicon -->
-    <link href="assets/images/favicon.png" rel="icon" type="image/png">
+    <title>@yield('title')</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <!-- Basic Page Needs
-        ================================================== -->
-    <title>Template</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
 
-    <!-- icons
-    ================================================== -->
-    <link rel="stylesheet" href="assets/css/icons.css">
-
-    <!-- CSS 
-    ================================================== --> 
-    <link rel="stylesheet" href="assets/css/uikit.css">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="stylesheet" href="assets/css/tailwind.css">  
+    <link href="{{ asset('assets/images/favicon.png') }}" rel="icon" type="image/png">
+    
+    {!! Minify::stylesheet([
+        '/assets/css/icons.css', 
+        '/assets/css/uikit.css', 
+        '/assets/css/style.css', 
+        '/assets/css/tailwind.css'
+    ])->withFullUrl() !!}
 
     @yield('css')
+    
 </head> 
 <body>
     @include('layouts.partials.sidenav')
@@ -38,7 +35,6 @@
         </div>
     </div>
  
-     <!-- For Night mode -->
      <script>
         (function (window, document, undefined) {
             'use strict';
@@ -65,14 +61,14 @@
         })(window, document);
     </script>
   
-    <!-- Javascript
-    ================================================== -->
-    <script src="assets/js/jquery-3.3.1.min.js"></script> 
-    <script src="assets/js/tippy.all.min.js"></script>
-    <script src="assets/js/uikit.js"></script>
-    <script src="assets/js/simplebar.js"></script>
-    <script src="assets/js/custom.js"></script>
-    <script src="assets/js/bootstrap-select.min.js"></script>
+    {!! Minify::javascript([
+        '/assets/js/jquery-3.3.1.min.js', 
+        '/assets/js/tippy.all.min.js', 
+        '/assets/js/uikit.js', 
+        '/assets/js/simplebar.js', 
+        '/assets/js/custom.js', 
+        '/assets/js/bootstrap-select.min.js'
+    ])->withFullUrl() !!}
     
     <script type="module" src="https://cdnjs.cloudflare.com/ajax/libs/ionicons/5.5.3/ionicons/ionicons.esm.js"></script>
     <script nomodule="" src="https://cdnjs.cloudflare.com/ajax/libs/ionicons/5.5.3/ionicons/ionicons.js"></script>
